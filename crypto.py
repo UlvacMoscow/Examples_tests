0fbf3769524b27a1a3316e49707e26ff7f967d35
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-12-16 21:59:03 +0300

    chekio encrypt

---
+++ crypto.py
@@ -0,0 +1,23 @@
+def to_encrypt(text, delta):
+    temp = ''
+    for char in text:
+        if char == ' ':
+            temp += char
+        if char.isalpha():
+            if ord(char) + delta < 97:
+                temp += chr((ord(char) + (26 + delta)))
+            elif ord(char) + delta > 122:
+                temp += chr((ord(char) - (26 - delta)))
+            else:
+                temp += chr((ord(char) + delta))
+    return temp
+
+print(ord('a'), ord('z'))
+print(chr(97), chr(122))
+
+print(to_encrypt('abc', 10))
+print(to_encrypt("a b c", 3))
+print(to_encrypt("a b c", -3))
+print(to_encrypt("simple text", 16))
+print(to_encrypt("important text", 10))
+print(to_encrypt("state secret", -13))
